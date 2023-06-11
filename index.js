10b021fbb2c772b2d97d4f09fdf331d26bdd73b8
sushil <sushilbajpai2003@gmail.com>
2023-06-11 13:51:35 +0530

    asdf

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const { noteRouter } = require("./routes/note.routes");
 
 const port=process.env.PORT
 const app=express();
-app.use(cors())
+const corsOptions ={
+    origin:'https://todo-mern-app.vercel.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
